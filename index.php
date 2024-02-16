c799785051b07022eb626d5dfae579811a446400
KyeongTaek <tommyholic@naver.com>
2024-02-16 19:23:18 +0900

    Day11

--- index.php
+++ index.php
@@ -3,16 +3,33 @@
   <head>
     <?php
       session_start();
+      $idx_num = 10;
     ?>
     
     <meta charset="utf-8">
     <title>php_project_bookstore</title>
     <link type="text/css" rel="stylesheet" href="./css/mainLayout.css" />
     
+   
+    
     <script type="text/javascript">
       function dropMenu() {
         
       }
+      
+      function coverShifter() {
+        var elem = document.getElementById("myId");
+        if(elem.outerHTML == "<") { // --
+          
+        }
+        else if(elem.outerHTML == ">") { // ++
+          
+        }
+        else { // ??
+          
+        }
+      }
+          
     </script>
   </head>
   
@@ -42,16 +59,53 @@
       
       <?php
         include __DIR__.'/templates/searchBar.html';
+        
+          function data_uri($binImage, $mime)
+          {
+            $base64 = base64_encode($binImage);
+            
+            return ('data:'.$mime.';base64,'.$base64);
+          }
       ?>
       
       <table>
         <tbody>
           <tr>
-            <td> <input type="button" name="left" value="<"/> </td>
-            <td> <img src="images/cosmos.jpg"> </td>
-            <td> <img src="images/courage_to_overcome_hatred.jpg"> </td>
-            <td> <img src="images/ji_dae_nulb_yat.jpg"> </td>
-            <td> <input type="button" name="right" value=">"/> </td>
+          
+            <?php
+              $sql = "SELECT id, book_cover FROM book_info";
+              
+              $result = $pdo->query($sql);
+              
+              $idx = 0;
+              foreach($result as $row) { // ex) $id_list[10] = 17, $img_list[10] = cosmos.jpg
+                $id_list[$idx] = $row['id'];
+                $img_list[$idx] = $row['book_cover'];
+                $idx = $idx + 1;
+              }
+            ?>
+            
+            
+            
+            <?php
+              echo '  <form method="post">';
+              echo '    <td> <input type="submit" name="left" value="<"/> </td>';
+              
+              echo '    <td> <a href="templates/bookInfo.php?id='.$id_list[$idx_num].'"> <img src="'.data_uri($img_list[$idx_num], 'image/png').'"/> </a> </td>';
+              echo '    <td> <a href="templates/bookInfo.php?id='.$id_list[$idx_num+1].'"> <img src="'.data_uri($img_list[$idx_num+1], 'image/png').'"/> </a> </td>'; 
+              echo '    <td> <a href="templates/bookInfo.php?id='.$id_list[$idx_num+2].'"> <img src="'.data_uri($img_list[$idx_num+2], 'image/png').'"/> </a> </td>';                           
+              
+              echo '    <td> <input type="submit" name="right" value=">"/> </td>';
+              echo '  </form>';
+              
+              if(array_key_exists('left', $_POST)) {
+                $idx_num = $idx_num - 1;
+              }
+              else if(array_key_exists('right', $_POST)) {
+                $idx_num = $idx_num + 1;
+              }
+                  
+            ?>
           </tr>
         </tbody>
       </table>
--- templates/trySigning.php
+++ templates/trySigning.php
@@ -1,3 +1,32 @@
 <?php
-  echo '<script type="text/javascript">location = "../index.php";</script>';
+  session_start();
+
+  try {
+    $pdo = new PDO('mysql:host=192.168.1.34; dbname=kt_php_bookstore; charset=utf8', 'root', 'sj4321');
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    
+    $sql = "select user_pw from `book_user` WHERE user_name = '".$_POST['user_id']."'"; // search db for typed id
+    $result = $pdo->query($sql);
+    $user = $result->fetch();
+    
+    if($user == "") { // no same id exists
+      $sql = "insert into `book_user` (`user_name`, `user_pw`, `user_admin`, `user_cash`, `user_addr`) VALUES ('".$_POST['user_id']."', '".$_POST['user_pw']."', 0, 0, '".$_POST['user_addr']."')";
+      echo $sql.'<br>';
+      
+      $statement = $pdo->prepare($sql);
+      
+      $statement->execute();
+      
+      echo 'SIGN UP SUCCESSFUL!!';
+    }  
+    else { // same id exists
+      echo 'SIGN UP FAILED!!';
+    }
+    
+  //  echo '<script type="text/javascript">history.go(-2);</script>';
+  } catch(PDOException $e) {
+    echo 'DB 접속 오류 : '.$e->getMessage().
+                          '<br>오류 발생 파일 : '.$e->getFile().
+                          '<br>오류 발생 행 : '.$e->getLine();
+  }  
 ?>
